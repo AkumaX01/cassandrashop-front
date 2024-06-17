1d2caf6f8b81bf1c5ba52512ade370906bafe39b
Ernesto Ramírez <ernestorb_@outlook.com>
2024-06-16 23:47:46 -0600

    Apuntar a v2 de la api (k8s)

--- src/app/utils/api.ts
+++ src/app/utils/api.ts
@@ -1,4 +1,4 @@
-const API_URL = `https://shop.ernestorb.com`;
+const API_URL = `https://v2.shop.ernestorb.com`;
 
 export function getUrl(path: string) {
   return new URL(path, API_URL).toString();
